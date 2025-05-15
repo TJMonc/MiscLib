e6174cdde9c1d37db8c1166f199c6afbcb180dce
Terrance Moncure <terrancemoncure873@gmail.com>
2025-05-14 21:20:08 -0500

    Added gitignore file.

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-cmake -G "MinGW Makefiles" -S . -B ./Build -D CMAKE_CXX_COMPILER
+cmake -G "MinGW Makefiles" -S . -B ./Build -D CMAKE_CXX_COMPILER=C:\msys64\mingw32(13.1.0)\bin\g++.exe
 cmake --build Build
